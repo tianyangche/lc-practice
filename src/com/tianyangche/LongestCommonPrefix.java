0c0d81c2115b785cad830ea437f1bc7a44fa75e8
tianyangche <tianyangche@gmail.com>
2018-10-24 00:06:54 -0700

    Problem 14. Longest Common Prefix.

---
+++ src/com/tianyangche/LongestCommonPrefix.java
@@ -0,0 +1,42 @@
+package com.tianyangche;
+
+public class LongestCommonPrefix {
+
+    /**
+     * Write a function to find the longest common prefix string amongst an array of strings.
+     *
+     * If there is no common prefix, return an empty string "".
+     *
+     * Example 1:
+     *
+     * Input: ["flower","flow","flight"]
+     * Output: "fl"
+     * Example 2:
+     *
+     * Input: ["dog","racecar","car"]
+     * Output: ""
+     * Explanation: There is no common prefix among the input strings.
+     * Note:
+     *
+     * All given inputs are in lowercase letters a-z.
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int index = 0;
+
+        while (index < strs[0].length()) {
+            char c = strs[0].charAt(index);
+            for (int i = 1; i < strs.length; i++) {
+                if (index >= strs[i].length() || strs[i].charAt(index) != c) {
+                    return strs[0].substring(0, index);
+                }
+            }
+            index++;
+        }
+        return strs[0];
+    }
+}
